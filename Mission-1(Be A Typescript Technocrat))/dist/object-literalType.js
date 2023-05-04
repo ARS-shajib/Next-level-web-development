801bb4d7e24dd8f9db06c6d557a63053b17d4fcd
ARS-shajib <abdurrahmanshajib253@gmail.com>
2023-05-04 07:24:00 +0600

    2.4 done

---
+++ Mission-1(Be A Typescript Technocrat))/dist/object-literalType.js
@@ -0,0 +1,9 @@
+"use strict";
+const user1 = {
+    company: "programming hero",
+    course: "both",
+    name: 'Abdur Rahman',
+    age: 24,
+    isMarried: false,
+};
+//user1.name = 'shajib'; // show error Cz readonly property.
